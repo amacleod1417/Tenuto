89ab12d87e9daafeea39d9f79aa1d9e513d10bc4
Ally MacLeod <155204136+amacleod1417@users.noreply.github.com>
2024-09-14 10:35:03 -0400

    spotify api integration

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -16,6 +16,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as messages from "../messages.js";
+import type * as spotify from "../spotify.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -27,6 +28,7 @@ import type * as messages from "../messages.js";
  */
 declare const fullApi: ApiFromModules<{
   messages: typeof messages;
+  spotify: typeof spotify;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
